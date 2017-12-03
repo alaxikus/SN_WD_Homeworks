a811607d354caf6941214922fb1e71d3cedc935a
Alaxikus <alaxikus.kokerska@gmail.com>
2017-12-03 18:58:55 +0100

    https://github.com/alaxikus/SN_WD_Homeworks

--- 20171203/python_secret_number.py
+++ 20171203/python_secret_number.py
@@ -1,12 +1,14 @@
+secret = "77"
+
 while True:
-    secret = 77
     guess = raw_input("Which number do you guess? ")
     if guess == secret:
         print "Congratulations! Your answer is correct."
-    if guess is not secret:
+        break
+    else:
         print "Your answer is incorrect."
         print "Try again? Type y or n"
-    if guess == "y":
-        print guess
-    if guess == "n":
-        break
+        if guess == "y":
+            print guess
+        if guess == "n":
+            break
